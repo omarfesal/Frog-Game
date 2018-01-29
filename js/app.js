59fd8b3539c853ad8027bedfcdc5b00201a2eda4
omar fesal <omarfesal4296@gmail.com>
2018-01-29 17:22:24 +0200

    add Player Class

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(xCooredinate , yCooredinate) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    
+    this.x = xCooredinate;
+    this.y = yCooredinate;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -21,14 +24,21 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Player of the game
+var Player = function(sprite, xCooredinate , yCooredinate) {
+    this.x = xCooredinate;
+    this.y = yCooredinate;
+    this.sprite = sprite;
+};
+
+// inheret all methods of enemy to player
+Player.prototype = Object.create(Enemy.prototype);
 
 
 
